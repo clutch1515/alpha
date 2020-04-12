3c16fe3476ab33095ad6ed2486a29fea9f31f52f
Peter Larsen <clutch1515@gmail.com>
2020-04-12 14:23:23 -0700

    alphadvantage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {top30Dax} from './top30Dax.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alpha = require('alphavantage')({key: 'A4OVD6QHD7ZFXIYD'});
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            label: '',
+            values: [],
+            x: '',
+            y: ''
+        }
+    }
+
+    componentDidMount() {
+        this.loadData();
+        console.log(this.state.data);
+    }
+
+    loadData() {
+        top30Dax.forEach((entry) => {
+            alpha.data.monthly_adjusted(entry.symbol).then(data => {
+                this.setState({data: [...this.state.data, ... [data]]})
+            });
+        });
+    }
+
+    calculateYearly() {
+        //TODO: parse monthly close points and calculate annual diff between Jan - Dec
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React with alphaAdvantage</h1>
+                </header>
+                  <form>
+                    <label>
+                        Ticker: 
+                        <input type="text" name="name" />
+                      </label>
+                      <input type="submit" value="Submit" />
+                  </form>
+                
+                <div className="App-intro">
+                    <h3>Top 30 DAX Companies</h3>
+                    {top30Dax.map((company,index) => {
+                        return <p key={index}>{company.name} :: {company.symbol}</p>
+                    })}
+                    <h3>Raw data provided by alphaAdvantage</h3>
+                    {this.state.data ? JSON.stringify(this.state.data[0]) : <span>No data</span>}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/quote.js
@@ -0,0 +1,3 @@
+const alpha = require('alphavantage')({key: 'A4OVD6QHD7ZFXIYD'});
+
+export const
